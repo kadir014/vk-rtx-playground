f2c31010516df54ee26d2293f416e849227a2a3b
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2026-08-03 18:01:23 +0300

    OBJ material loading

--- include/lava/loaders/obj.h
+++ include/lava/loaders/obj.h
@@ -13,7 +13,9 @@
  */
 
 
-// TODO: Use glm's vectors
+#define LV_OBJ_MTL_NAME_LENGTH 64
+#define LV_OBJ_MTL_MAP_FILEPATH_LENGTH 256
+
 
 typedef struct {
     float x;
@@ -30,16 +32,18 @@ typedef struct {
 /**
  * @brief Face definition with property IDs in OBJ file.
  * 
+ * Currently only supports triangulated faces.
+ * 
  * f v/n/uv [...]
  */
 typedef struct {
-    long vertex_ids[3];
-    long normal_ids[3];
-    long uv_ids[3];
+    int64_t vertex_ids[3];
+    int64_t normal_ids[3];
+    int64_t uv_ids[3];
 } lvOBJFace;
 
 /**
- * @brief Simple triangle face.
+ * @brief Single triangular face.
  */
 typedef struct {
     lvOBJVec3 vertices[3];
@@ -48,20 +52,22 @@ typedef struct {
 } lvOBJTri;
 
 /**
- * @brief Triangular mesh geometry defined in OBJ file.
+ * @brief Triangular mesh geometry.
  */
 typedef struct {
+    char material_name[LV_OBJ_MTL_NAME_LENGTH];
     lvArray tris;
 } lvOBJMesh;
 
 /**
- * @brief Type representing loaded Wavefront OBJ file.
+ * @brief Type representing a loaded Wavefront OBJ file.
  */
 typedef struct {
     /*
         Temporary state used by the loader.
     */
     char *current;
+    char current_mtl_name[LV_OBJ_MTL_NAME_LENGTH];
     lvArray vertices;
     lvArray normals;
     lvArray uvs;
@@ -71,24 +77,89 @@ typedef struct {
         Public members.
     */
     bool loaded;
+    lvArray materials;
     lvOBJMesh mesh;
 } lvOBJ;
 
 /**
- * @brief Load OBJ from source null-terminated string.
+ * @brief Material definition in a MTL file using extended PBR properties.
+ */
+typedef struct {
+    char name[LV_OBJ_MTL_NAME_LENGTH];
+
+    float metallic;
+    char metallic_map[LV_OBJ_MTL_MAP_FILEPATH_LENGTH];
+
+    float roughness;
+    char roughness_map[LV_OBJ_MTL_MAP_FILEPATH_LENGTH];
+
+    float sheen;
+    char sheen_map[LV_OBJ_MTL_MAP_FILEPATH_LENGTH];
+
+    float clearcoat_thickness;
+    float clearcoat_roughness;
+
+    lvOBJVec3 emissive;
+    char emissive_map[LV_OBJ_MTL_MAP_FILEPATH_LENGTH];
+
+    float anisotropy;
+    float anisotropy_rotation;
+
+    char normal_map[LV_OBJ_MTL_MAP_FILEPATH_LENGTH];
+
+    float ior;
+} lvOBJMaterialPBR;
+
+/**
+ * @brief Load Wavefornt OBJ from source null-terminated string.
  * 
- * @param source OBJ content.
+ * Use `lvObj.loaded` member to see if initialization was successful before using.
+ * 
+ * @param source Null-terminated Wavefront OBJ content.
  * @return lvOBJ 
  */
 lvOBJ lvOBJ_load_raw(char *source);
 
 /**
- * @brief Load OBJ from file.
+ * @brief Load Wavefront OBJ from file.
+ * 
+ * Use `lvObj.loaded` member to see if initialization was successful before using.
  * 
  * @param filepath Path to Wavefront OBJ file.
  * @return lvOBJ 
  */
 lvOBJ lvOBJ_load(const char *filepath);
+
+/**
+ * @brief Load Wavefront OBJ from file with its material libary.
+ * 
+ * Use `lvObj.loaded` member to see if initialization was successful before using.
+ * 
+ * @param obj_filepath Path to Wavefront OBJ file.
+ * @param mtl_filepath Path to material library file.
+ * @return lvOBJ 
+ */
+lvOBJ lvOBJ_load_with_mtl(const char *obj_filepath, const char *mtl_filepath);
+
+/**
+ * @brief Free OBJ instance.
+ * 
+ * It's safe to pass `NULL` to this function.
+ * 
+ * @param obj OBJ instance to free.
+ */
+void lvOBJ_free(lvOBJ *obj);
+
+/**
+ * @brief Get loaded material with given name.
+ * 
+ * Returns `NULL` if failed to fetch.
+ * 
+ * @param obj Loaded OBJ instance.
+ * @param name Material name defined in MTL file.
+ * @return lvOBJMaterialPBR *
+ */
+lvOBJMaterialPBR *lvOBJ_get_material(lvOBJ *obj, const char *name);
 
 
 #endif // LV_LOADER_WAVEFRONT_OBJ_H
--- src/lava/loaders/obj.c
+++ src/lava/loaders/obj.c
@@ -2,6 +2,90 @@
 #include "lava/core/io.h"
 
 
+/*
+    The reason these 'fast' parsing functions are faster than standard library's
+    alternatives is that they only work on a very small subset of numerals.
+
+    For example they only suppert decimal representation, no scientific notation,
+    no constants like NaN or inf, no overflow checking etc...
+
+    Since OBJ files also store only those boring subset of numbers, we can
+    utilize these faster alternatives.
+*/
+
+/**
+ * @brief Fast ASCII string to 32-bit float.
+ * 
+ * Advances the given pointer to string.
+ * 
+ * @param p Pointer to string (char array).
+ * @return float 
+ */
+static inline float fast_str2f(const char **p) {
+    const char *s = *p;
+
+    float sign = 1.0f;
+    if (*s == '-') {
+        sign = -1.0f;
+        s++;
+    }
+    else if (*s == '+') {
+        s++;
+    }
+
+    // INTEGER PART
+    float value = 0.0f;
+    while (*s >= '0' && *s <= '9') {
+        value = value * 10.0f + (*s - '0');
+        s++;
+    }
+
+    // FRACTIONAL PART
+    if (*s == '.') {
+        s++;
+        float scale = 0.1f;
+        while (*s >= '0' && *s <= '9') {
+            value += (*s - '0') * scale;
+            // Each successive digit contributes one-tenth as much.
+            // same as moving to next decimal place.
+            scale *= 0.1f;
+            s++;
+        }
+    }
+
+    *p = s;
+    return value * sign;
+}
+
+/**
+ * @brief Fast ASCII string to 64-bit signed integer.
+ * 
+ * Advances the given pointer to string.
+ * 
+ * @param p Pointer to string (char array).
+ * @return int64_t 
+ */
+static inline int64_t fast_str2i64(const char **p) {
+    const char *s = *p;
+
+    int64_t sign = 1;
+    if (*s == '-') {
+        sign = -1;
+        s++;
+    }
+
+    int64_t value = 0;
+    while (*s >= '0' && *s <= '9') {
+        // ASCII char - ASCII char = integer value of the char
+        value = value * 10 + (*s - '0');
+        s++;
+    }
+
+    *p = s;
+    return value * sign;
+}
+
+
 static inline bool is_whitespace(char chr) {
     return (chr == ' ' || chr == '\t' || chr == '\r' || chr == '\n');
 }
@@ -26,66 +110,6 @@ static inline void skip_line(lvOBJ *obj) {
     ADVANCE;
 }
 
-
-static inline float fast_atof(const char **p) {
-    const char *s = *p;
-    float sign = 1.0f;
-    if (*s == '-') { sign = -1.0f; s++; }
-    else if (*s == '+') { s++; }
-
-    float value = 0.0f;
-    while (*s >= '0' && *s <= '9') {
-        value = value * 10.0f + (*s - '0');
-        s++;
-    }
-
-    if (*s == '.') {
-        s++;
-        float frac = 0.0f;
-        float scale = 1.0f;
-        while (*s >= '0' && *s <= '9') {
-            frac = frac * 10.0f + (*s - '0');
-            scale *= 10.0f;
-            s++;
-        }
-        value += frac / scale;
-    }
-
-    *p = s;
-    return value * sign;
-}
-
-static inline int fast_atoi(const char **p) {
-    const char *s = *p;
-    int sign = 1;
-    if (*s == '-') { sign = -1; s++; }
-
-    int value = 0;
-    while (*s >= '0' && *s <= '9') {
-        value = value * 10 + (*s - '0');
-        s++;
-    }
-    *p = s;
-    return value * sign;
-}
-
-static inline float parse_float(lvOBJ *obj) {
-    char *end;
-    float value = strtof(obj->current, &end);
-    obj->current = end;
-    return value;
-
-    //return fast_atof(&obj->current);
-}
-
-static inline long parse_long(lvOBJ *obj) {
-    char *end;
-    long value = strtol(obj->current, &end, 10);
-    obj->current = end;
-    return value;
-    //return (long)fast_atoi(&obj->current);
-}
-
 static inline void parse_vertex(lvOBJ *obj) {
     /*
         Syntax:
@@ -95,13 +119,13 @@ static inline void parse_vertex(lvOBJ *obj) {
     ADVANCE; // skip v
 
     skip_whitespace(obj);
-    float x = parse_float(obj);
+    float x = fast_str2f(&obj->current);
 
     skip_whitespace(obj);
-    float y = parse_float(obj);
+    float y = fast_str2f(&obj->current);
 
     skip_whitespace(obj);
-    float z = parse_float(obj);
+    float z = fast_str2f(&obj->current);
 
     lvOBJVec3 vertex = {x, y, z};
     lvArray_add(&obj->vertices, &vertex);
@@ -116,13 +140,13 @@ static inline void parse_normal(lvOBJ *obj) {
     ADVANCE; ADVANCE; // skip vn
 
     skip_whitespace(obj);
-    float x = parse_float(obj);
+    float x = fast_str2f(&obj->current);
 
     skip_whitespace(obj);
-    float y = parse_float(obj);
+    float y = fast_str2f(&obj->current);
 
     skip_whitespace(obj);
-    float z = parse_float(obj);
+    float z = fast_str2f(&obj->current);
 
     lvOBJVec3 normal = {x, y, z};
     lvArray_add(&obj->normals, &normal);
@@ -137,10 +161,10 @@ static inline void parse_uv(lvOBJ *obj) {
     ADVANCE; ADVANCE; // skip vt
 
     skip_whitespace(obj);
-    float x = parse_float(obj);
+    float x = fast_str2f(&obj->current);
 
     skip_whitespace(obj);
-    float y = parse_float(obj);
+    float y = fast_str2f(&obj->current);
 
     lvOBJVec2 uv = {x, y};
     lvArray_add(&obj->uvs, &uv);
@@ -158,33 +182,33 @@ static inline void parse_face(lvOBJ *obj) {
 
     skip_whitespace(obj);
 
-    long v0 = parse_long(obj);
+    int64_t v0 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long uv0 = parse_long(obj);
+    int64_t uv0 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long n0 = parse_long(obj);
+    int64_t n0 = fast_str2i64(&obj->current);
 
     skip_whitespace(obj);
 
-    long v1 = parse_long(obj);
+    int64_t v1 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long uv1 = parse_long(obj);
+    int64_t uv1 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long n1 = parse_long(obj);
+    int64_t n1 = fast_str2i64(&obj->current);
 
     skip_whitespace(obj);
 
-    long v2 = parse_long(obj);
+    int64_t v2 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long uv2 = parse_long(obj);
+    int64_t uv2 = fast_str2i64(&obj->current);
 
     ADVANCE;
-    long n2 = parse_long(obj);
+    int64_t n2 = fast_str2i64(&obj->current);
 
     lvOBJFace face = {
         .vertex_ids = {v0, v1, v2},
@@ -216,6 +240,86 @@ static void parse_obj(lvOBJ *obj) {
 
         else if (*obj->current == 'f') {
             parse_face(obj);
+        }
+        
+        else {
+            skip_line(obj);
+        }
+    }
+}
+
+static inline void parse_newmtl(lvOBJ *obj) {
+    /*
+        Syntax:
+        newmtl string
+    */
+
+    ADVANCE; ADVANCE; ADVANCE; ADVANCE; ADVANCE; ADVANCE; // skip newmtl
+
+    skip_whitespace(obj);
+
+    char name[LV_OBJ_MTL_NAME_LENGTH];
+    size_t i = 0;
+    while (
+        *obj->current != '\0' &&
+        !is_whitespace(*obj->current) &&
+        i < LV_OBJ_MTL_NAME_LENGTH
+    ) {
+        name[i] = *obj->current;
+
+        ADVANCE;
+        i++;
+    }
+
+    lvOBJMaterialPBR mat = {0};
+    memcpy(mat.name, name, LV_OBJ_MTL_NAME_LENGTH);
+    memcpy(obj->current_mtl_name, name, LV_OBJ_MTL_NAME_LENGTH);
+
+    lvArray_add(&obj->materials, &mat);
+}
+
+static inline void parse_roughness(lvOBJ *obj) {
+    /*
+        Syntax:
+        Pr float
+    */
+
+    ADVANCE; ADVANCE; // skip Pr
+
+    skip_whitespace(obj);
+
+    float x = fast_str2f(&obj->current);
+
+    printf("roughness: %f\n", x);
+
+    lvOBJMaterialPBR *mat = lvOBJ_get_material(obj, obj->current_mtl_name);
+    mat->roughness = x;
+}
+
+static void parse_mtl(lvOBJ *obj) {
+    while (*obj->current != '\0') {
+        skip_whitespace(obj);
+
+        if (*obj->current == '#') {
+            skip_line(obj);
+        }
+
+        else if (
+            *(obj->current + 0) == 'n' &&
+            *(obj->current + 1) == 'e' &&
+            *(obj->current + 2) == 'w' &&
+            *(obj->current + 3) == 'm' &&
+            *(obj->current + 4) == 't' &&
+            *(obj->current + 5) == 'l'
+        ) {
+            parse_newmtl(obj);
+        }
+
+        else if (
+            *(obj->current + 0) == 'P' &&
+            *(obj->current + 1) == 'r'
+        ) {
+            parse_roughness(obj);
         }
         
         else {
@@ -279,8 +383,57 @@ lvOBJ lvOBJ_load(const char *filepath) {
 
     lvOBJ obj = lvOBJ_load_raw(content.data);
     obj.loaded = true;
+    obj.materials = lvArray_new(sizeof(lvOBJMaterialPBR));
 
     LV_FREE(content.data);
     
     return obj;
+}
+
+lvOBJ lvOBJ_load_with_mtl(const char *obj_filepath, const char *mtl_filepath) {
+    lvOBJ obj = lvOBJ_load(obj_filepath);
+    
+    lvFileContent mtl_content = lv_read_file_raw(mtl_filepath);
+    if (!mtl_content.data) {
+        obj.loaded = false;
+        return obj;
+    }
+
+    obj.current = mtl_content.data;
+    parse_mtl(&obj);
+
+    LV_FREE(mtl_content.data);
+
+    return obj;
+}
+
+void lvOBJ_free(lvOBJ *obj) {
+    if (!obj) return;
+
+    lvArray_free(&obj->mesh.tris);
+    lvArray_free(&obj->materials);
+}
+
+lvOBJMaterialPBR *lvOBJ_get_material(lvOBJ *obj, const char *name) {
+    if (!obj->loaded || !lvArray_valid(&obj->materials)) {
+        return NULL;
+    }
+
+    bool found = false;
+    lvOBJMaterialPBR *found_mat;
+    for (size_t i = 0; i < obj->materials.size; i++) {
+        lvOBJMaterialPBR *mat = LV_ARRAY_PTR_AT(&obj->materials, i, lvOBJMaterialPBR);
+
+        if (strcmp(mat->name, name) == 0) {
+            found = true;
+            found_mat = mat;
+            break;
+        }
+    }
+
+    if (found) {
+        return found_mat;
+    }
+
+    return NULL;
 }
